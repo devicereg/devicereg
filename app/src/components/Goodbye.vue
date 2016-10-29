2bc4052ffe7371389c548e41bfa6cd96dccf41db
Moritz Wachter <moritzwachter@yahoo.de>
2016-10-29 14:10:09 +0200

    added new test component for the router

---
+++ app/src/components/Goodbye.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="goodbye">
+    {{ msg }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'goodbye',
+  data () {
+    return {
+      msg: 'Goodbye'
+    }
+  }
+}
+</script>
