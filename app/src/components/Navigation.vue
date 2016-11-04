e392ae7243d39dea20b72385b03187bff7a9754b
Stilmittel <strohbrain@yahoo.de>
2016-11-04 11:43:09 +0100

    Feature/105-vuejs-router (#1)
    * added new test component for the router
    
    * added Navigation component and made the "Hello.vue"-styles global by adding them to App.vue
    
    * added vue-router and created an example navigation with 2 Components
    
    * readded styles to app

---
+++ app/src/components/Navigation.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="navigation">
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/bye">Goodbye</router-link></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'navigation',
+  data () {
+    return {}
+  }
+}
+</script>
