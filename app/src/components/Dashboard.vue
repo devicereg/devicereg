51812a741bb189dec1254f35a7d755c26f89b9f0
Moritz Wachter <moritzwachter@yahoo.de>
2016-11-12 12:01:09 +0100

    changed order to template, script, style to be consistent

--- app/src/components/Dashboard.vue
+++ app/src/components/Dashboard.vue
@@ -1,31 +1,9 @@
-<style lang="scss">
-
-/**
- *--------------------------------------------------------------------------
- *	Vue-component for user dashboard
- *--------------------------------------------------------------------------
- *
- * 	This is the Vue-component for the user dashboard.
- *
- * 	This file includes the HTML template, sass-stylesheet 
- * 	and javascript for the user dashboard.
- *
- */
-
-.dashboard{
-	h1{
-		font-size:52px;
-	}
-}
-
-</style>
-
 <template>
 	<div class="container dashboard">
 		<div class="row">
 			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 				<div>
-					<h1>Welcome {{ username }}</h1> 
+					<h1>Welcome {{ username }}</h1>
 					<h2>This is your Dashboard</h2>
 				</div>
 			</div>
@@ -34,7 +12,6 @@
 </template>
 
 <script>
-	
 	import auth from '../auth'
 
 	export default {
@@ -46,3 +23,12 @@
 	  }
 	}
 </script>
+
+<style lang="scss">
+  .dashboard{
+    h1{
+      font-size:52px;
+    }
+  }
+
+</style>
