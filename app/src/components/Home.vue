738181e2ab605334bed9f5a87497b3243e019590
Moritz Wachter <moritzwachter@yahoo.de>
2016-11-12 13:02:49 +0100

    merged texts from feature/130-welcome-page into Home.vue

--- app/src/components/Home.vue
+++ app/src/components/Home.vue
@@ -9,12 +9,18 @@
 		</div>
 		<div class="row">
 			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 home-header-titles">
-				<h1>Welcome to DeviceR</h1>
-				<h2>At vero eos et accusam et justo duo dolores et ea rebum.</h2>
-
-				<p>
-				Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
-				</p>
+				<h1>Willkommen bei DeviceR!</h1>
+        <p>Sie haben die Möglichkeit, einen Benutzer anzulegen und anschließend ein Durchflussmessgerät zu registrieren oder
+          sich mit Ihrem bestehenden Profilanzumelden um weitere Geräte hinzuzufügen oder diese zu verwalten.
+        </p>
+        <h3>Ihr Vorteil</h3>
+        <p>
+          Der <b>Gewährleistungszeitraum</b> registrierter Geräte verlängert   sich auf <b>24 Monate</b>.
+        </p>
+        <p>
+          Ein weiterer Nutzen unseres Services: Unsere <b>Erinnerungsfunktion</b> macht sie auf
+          anstehende Wartungen oder Kalibrierungen Ihrer registrierten Durchflussmessgeräte aufmerksam.
+        </p>
 			</div>
 		</div>
 		<div class="row">
