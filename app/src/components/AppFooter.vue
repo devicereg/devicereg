f8f1555b7ce241eef001e1e418c8669c2059f901
Michael Baumert <[E-Mail-Adresse>
2016-11-20 14:19:19 +0100

    added the footer but something went wrong

---
+++ app/src/components/AppFooter.vue
@@ -0,0 +1,35 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <ul class="nav navbar-nav">
+        <li><router-link to="/">Home</router-link></li>
+        <li v-if="user.authenticated">
+          <router-link to="/dashboard">Dashboard</router-link>
+        </li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li v-if="!user.authenticated">
+          <router-link to="/registrieren">Registrieren</router-link>
+        </li>
+        <li><router-link to="/kontakt">Kontakt</router-link></li>
+        <li class="logout-user" v-if="user.authenticated" @click="logout()">
+          <router-link to="/login">Abmelden</router-link>
+        </li>
+      </ul>
+    </div>
+  </footer>
+</template>
+<script>
+  export default{
+    name: 'app-footer'
+  }
+</script>
+<style lang="scss">
+  .footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 60px;
+    background-color: #f5f5f5;
+  }
+</style>
