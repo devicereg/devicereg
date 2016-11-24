1592c67390b4ccaad1402ad19a148df6575c9cdd
micken42 <michael_baumert@gmx.de>
2016-11-24 14:53:47 +0100

    Feature/164-translation (#7)
    * added translations to the app
    
    * updated the translation component
    
    * changed the translation keys
    
    * added color and language selection to the navbar

--- app/src/App.vue
+++ app/src/App.vue
@@ -14,6 +14,7 @@ import AppFooter from './components/AppFooter'
 import Dashboard from './components/Dashboard'
 import Bootstrap from './components/Bootstrap'
 import Kontakt from './components/Kontakt'
+import Translations from './components/Translations'
 
 
 export default {
@@ -24,7 +25,8 @@ export default {
     AppFooter,
     Dashboard,
     Bootstrap,
-    Kontakt
+    Kontakt,
+    Translations
   }
 }
 </script>
