172eba8f891bf5c973eec9cb43550f14ed18cded
Moritz Wachter <moritz.wachter@exozet.com>
2016-11-29 17:57:47 +0100

    small cleanup in index.js

--- app/src/auth/index.js
+++ app/src/auth/index.js
@@ -1,12 +1,12 @@
 import {router} from '../main'
 
-var os = require('os')
+var os = require('os');
 
-const API_URL = 'http://' + os.hostname() + ':3001/'
-const LOGIN_URL = API_URL + 'sessions/create/'
-const SIGNUP_URL = API_URL + 'users/'
-const UPDATE_URL = API_URL + 'user/update'
-const DELETE_URL = API_URL + 'user/delete'
+const API_URL = 'http://' + os.hostname() + ':3001/';
+const LOGIN_URL = API_URL + 'sessions/create/';
+const SIGNUP_URL = API_URL + 'users/';
+const UPDATE_URL = API_URL + 'user/update';
+const DELETE_URL = API_URL + 'user/delete';
 
 export default {
 	user: { authenticated: false },
@@ -15,17 +15,15 @@ export default {
 	 * Method for user login
 	 *
 	 * @param      {object}  context   The context
-	 * @param      {json}    creds     The creds
+	 * @param      {JSON}    creds     The creds
 	 * @param      {string}  redirect  The redirect
 	 */
 	login(context, creds, redirect)
 	{
 		context.$http.post(LOGIN_URL, creds).then((response) => {
 
-			localStorage.setItem('id_token', response.data.id_token)
-			// localStorage.setItem('username', creds.username)
-
-			this.user.authenticated = true
+			localStorage.setItem('id_token', response.data.id_token);
+			this.user.authenticated = true;
 
 			if(redirect)
 			{
@@ -41,16 +39,15 @@ export default {
 	 * Method for user registration
 	 *
 	 * @param      {object}  context   The context
-	 * @param      {json}  	 creds     The creds
+	 * @param      {JSON}  	 creds     The creds
 	 * @param      {string}  redirect  The redirect
 	 */
 	signup(context, creds, redirect)
 	{
 	    context.$http.post(SIGNUP_URL, creds).then((response) => {
 
-			localStorage.setItem('id_token', response.data.id_token)
-
-	      	this.user.authenticated = true
+			localStorage.setItem('id_token', response.data.id_token);
+	      	this.user.authenticated = true;
 
 			if(redirect)
 			{
@@ -67,17 +64,14 @@ export default {
 	 * Method for user update
 	 *
 	 * @param      {object}  context   The context
-	 * @param      {json}  	 creds     The creds
+	 * @param      {JSON}  	 creds     The creds
 	 * @param      {string}  redirect  The redirect
 	 */
 	update(context, creds, redirect)
 	{
 	    context.$http.post(UPDATE_URL, creds).then((response) => {
 
-	    	console.log(response);
-			localStorage.setItem('id_token', response.data.id_token)
-
-	      	this.user.authenticated = true
+			//@TODO implement UPDATE method
 
 			if(redirect)
 			{
@@ -93,12 +87,11 @@ export default {
 	 * Method for user delete
 	 *
 	 * @param      {object}  context   The context
-	 * @param      {json}  	 creds     The creds
-	 * @param      {string}  redirect  The redirect
+ 	 * @param      {int}   	 id     	The creds
 	 */
-	delete(context, creds)
+	delete(context, id)
 	{
-	    context.$http.post(DELETE_URL, creds).then((response) => {
+	    context.$http.post(DELETE_URL, id).then((response) => {
 
 	    	console.log(response.data.message);
 	    	this.logout()
@@ -115,9 +108,9 @@ export default {
 	 */
 	logout()
 	{
-		localStorage.removeItem('id_token')
-		localStorage.removeItem('username')
-		this.user.authenticated = false
+		localStorage.removeItem('id_token');
+		localStorage.removeItem('username');
+		this.user.authenticated = false;
 
 		router.push("/")
 	},
@@ -129,16 +122,9 @@ export default {
 	 */
 	checkAuth()
 	{
-		var jwt = localStorage.getItem('id_token')
+		var jwt = localStorage.getItem('id_token');
 
-		if(jwt)
-		{
-			this.user.authenticated = true
-		}
-		else
-		{
-			this.user.authenticated = false
-		}
+		this.user.authenticated = !!jwt;
 	},
 
 	/**
