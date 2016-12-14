be9f2fd533695adac7b0cdfb1ce32615c465ab27
Evgenij Bin <s0545820@htw-berlin.de>
2016-12-14 23:13:06 +0100

    added category filter to MyDevices Page

--- app/src/components/MyDevices.vue
+++ app/src/components/MyDevices.vue
@@ -3,10 +3,17 @@
     <div class="row">
       <div class="col-sm-12 text-left">
         <div class="row">
-          <div class="col-sm-6">
+          <div class="col-sm-8">
             <h2> {{$t("MyDevices.title")}} </h2>
           </div>
-          <div class="col-sm-6">
+          <div class="col-sm-2 form-group-sm text-right">
+            <select id="cat_filter" class="form-control" v-model="cat_filter">
+              <option id="option_placeholder" value="placeholder" disabled>{{ $t("MyDevices.filter_by") }}</option>
+              <option value="all">{{ $t("MyDevices.all_categories") }}</option>
+              <option v-bind:value="cat.name" v-for="cat in categories">{{cat.name}}</option>
+            </select>
+          </div>
+          <div class="col-sm-2">
             <router-link to="/device/create" id="add-button" class="btn btn-primary pull-right">
               <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> &nbsp; {{ $t("MyDevices.add_button") }}
             </router-link>
@@ -23,7 +30,8 @@
             </tr>
           </thead>
           <tbody>
-          <tr v-for="device in devices">
+          <tr v-for="device in devices" v-if="device.category == cat_filter
+                            || cat_filter == 'all' || cat_filter == 'placeholder'">
             <td>{{device.technology}}</td>
             <td>{{device.description_data}}</td>
             <td>{{device.category}}</td>
@@ -48,7 +56,7 @@
 <style lang="scss">
   @import '../styles/_colors';
 
-  #add-button {
+  #add-button,#cat_filter {
     margin-top: 2em;
   }
 
@@ -59,6 +67,9 @@
   a {
     color: $primary-link-color;
   }
+  #option_placeholder {
+    display: none;
+  }
 </style>
 <script>
   import mockDevices from "./MockData/devices.json"
@@ -68,7 +79,12 @@
     data () {
       return {
         selected: '0',
-        devices: mockDevices
+        devices: mockDevices,
+        categories: [
+          {id: 1, name: 'Durchfluss'},
+          {id: 2, name: 'EigeneKategorie'}
+        ],
+        cat_filter: 'placeholder'
       }
     },
     methods: {
