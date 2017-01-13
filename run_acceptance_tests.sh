b471f714a05e837e29c38e74bdd4b6940186f006
Michael Baumert <[E-Mail-Adresse>
2017-01-13 06:44:38 +0100

    Acceptance tests should now be executed correctly on the server

--- run_acceptance_tests.sh
+++ run_acceptance_tests.sh
@@ -5,7 +5,7 @@ BROWSER=${BROWSER:-"firefox"}
 OUTPUTDIR=${OUTPUTDIR:-"app/static/test-output"}
 ROBOT_TESTS=${ROBOT_TESTS:-"robot-tests/devicer-testsuite/"}
 CONTAINER_NAME=${CONTAINER_NAME:-"devicereg_test"}
-
+cd devicereg
 docker run -e USER=${USER} -e URL=${URL} -e BROWSER=${BROWSER} --rm \
     -v $(pwd)/${ROBOT_TESTS}:/robot-tests/devicer-testsuite/ \
     -v $(pwd)/${OUTPUTDIR}:/robot-test-output  -ti ${CONTAINER_NAME} run.sh
