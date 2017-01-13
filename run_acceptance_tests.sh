95734347b6aa4678cd88182996aad9ce576e3d3b
Michael Baumert <[E-Mail-Adresse>
2017-01-13 06:37:33 +0100

    Test after deployment are now running in firefox

--- run_acceptance_tests.sh
+++ run_acceptance_tests.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 URL=${URL:-"http://devicereg-frontend.f4.htw-berlin.de/#/"}
-USER=${USERNAME:-"mustermax@mail.de"}
-BROWSER=${BROWSER:-"chrome"}
-OUTPUTDIR=${OUTPUTDIR:-"devicereg/app/static/test-output"}
-ROBOT_TESTS=${ROBOT_TESTS:-"devicereg/robot-tests/devicer-testsuite/"}
+USER=${USERNAME:-"mustermaax@mail.de"}
+BROWSER=${BROWSER:-"firefox"}
+OUTPUTDIR=${OUTPUTDIR:-"app/static/test-output"}
+ROBOT_TESTS=${ROBOT_TESTS:-"robot-tests/devicer-testsuite/"}
 CONTAINER_NAME=${CONTAINER_NAME:-"devicereg_test"}
 
 docker run -e USER=${USER} -e URL=${URL} -e BROWSER=${BROWSER} --rm \
     -v $(pwd)/${ROBOT_TESTS}:/robot-tests/devicer-testsuite/ \
-    -v $(pwd)/${OUTPUTDIR}:/robot-test-output  -ti ${CONTAINER_NAME}
+    -v $(pwd)/${OUTPUTDIR}:/robot-test-output  -ti ${CONTAINER_NAME} run.sh
 exit
