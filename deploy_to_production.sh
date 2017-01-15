96ceb8084cd145bc9cbb216f5c625c27e8c608c7
Michael Baumert <[E-Mail-Adresse>
2017-01-15 21:45:08 +0100

    Updated and fixed some deployment errors

--- deploy_to_production.sh
+++ deploy_to_production.sh
@@ -3,12 +3,15 @@ cd devicereg
 echo "####### pulling from git #######"
 git checkout -- .
 git pull
-echo "####### building the new docker images #######"
-docker-compose build
 echo "####### shut down the app and remove container #######"
 docker-compose down
+echo "####### remove the old docker images and container #######"
+docker rm $(docker ps -aq)
+docker rmi htw-berlin/devicer-robot htw-berlin/devicer-web
+echo "####### building the new docker images #######"
+docker-compose build
 echo "####### starting the app #######"
 docker-compose up -d
-echo "####### delete old dangling images #######"
-docker rmi $(docker images -f "dangling=true" -q)
+#echo "####### delete old dangling images #######"
+#docker rmi $(docker images -f "dangling=true" -q)
 exit
