5fb64041e7ab0e8a48f88f717f3c10163e08fe2d
Michael Baumert <[E-Mail-Adresse>
2017-01-16 00:30:09 +0100

    Fixed test execution error on circleci machine

--- run_acceptance_tests.sh
+++ run_acceptance_tests.sh
@@ -12,8 +12,9 @@ ROBOT_IMAGE_NAME=${ROBOT_IMAGE_NAME:-"test"}
 ROBOT_CONTAINER_NAME=${ROBOT_CONTAINER_NAME:-"devicereg_test_1"}
 COMPOSE_NETWORK_NAME=${COMPOSE_NETWORK_NAME:-"devicereg_default"}
 
+# add --rm for local robot test execution
 docker run -e ROBOT_USER=${ROBOT_USER} -e WEB_IMAGE_NAME=${WEB_IMAGE_NAME} \
-    -e WEB_IMAGE_PORT=${WEB_IMAGE_PORT} -e ROBOT_BROWSER=${ROBOT_BROWSER} --rm \
+    -e WEB_IMAGE_PORT=${WEB_IMAGE_PORT} -e ROBOT_BROWSER=${ROBOT_BROWSER} \
     -v $(pwd)/${ROBOT_TESTS}:/robot-tests/devicer-testsuite/ -v $(pwd)/${ROBOT_OUTPUTDIR}:/robot-test-output \
     --link ${WEB_CONTAINER_NAME}:${WEB_IMAGE_NAME} --link ${ROBOT_CONTAINER_NAME}:${ROBOT_IMAGE_NAME} -ti \
     --net ${COMPOSE_NETWORK_NAME} ${ROBOT_IMAGE_NAME} run.sh
