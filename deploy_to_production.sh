f1ebacdba1fea4bf0a9dc2fbba96c4a916de97dd
Michael Baumert <[E-Mail-Adresse>
2017-01-15 23:19:20 +0100

    Configured robot tests to run in compose network on circleci machines

--- deploy_to_production.sh
+++ deploy_to_production.sh
@@ -7,11 +7,14 @@ echo "####### shut down the app and remove container #######"
 docker-compose down
 echo "####### remove the old docker images and container #######"
 docker rm $(docker ps -aq)
+docker rmi $(docker images -q -f dangling=true)
 docker rmi htw-berlin/devicer-robot htw-berlin/devicer-web
 echo "####### building the new docker images #######"
 docker-compose build
 echo "####### starting the app #######"
 docker-compose up -d
-#echo "####### delete old dangling images #######"
-#docker rmi $(docker images -f "dangling=true" -q)
+# Delete all stopped containers
+docker ps -q -f status=exited
+# Delete all dangling (unused) images
+
 exit
