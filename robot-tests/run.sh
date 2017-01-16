6276528e96fcac087e0f21c154bdb7a15784c09e
Michael Baumert <[E-Mail-Adresse>
2017-01-17 00:42:42 +0100

    Added the device creation robot test and fixed deployment

--- robot-tests/run.sh
+++ robot-tests/run.sh
@@ -12,7 +12,7 @@ DEFAULT_WEB_IMAGE_PORT="8080"
 DEFAULT_ROBOT_LOG_LEVEL="INFO" # Available levels: TRACE, DEBUG, INFO (default), WARN, NONE (no logging)
 DEFAULT_ROBOT_TESTS="/robot-tests/devicer-testsuite/"
 DEFAULT_ROBOT_USER="mustermax@htw-berlin.de"
-DEFAULT_ROBOT_BROWSER="chrome"
+DEFAULT_ROBOT_BROWSER="firefox"
 DEFAULT_ROBOT_OUTPUTDIR="/robot-test-output"
 
 # Use default if none specified as env var
