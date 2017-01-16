56112845fef3a3fb58579409345f8587a6378023
Michael Baumert <[E-Mail-Adresse>
2017-01-16 01:14:07 +0100

    run_acceptance_tests.sh can now be used for local robot test execution

--- run_acceptance_tests.sh
+++ run_acceptance_tests.sh
@@ -9,14 +9,10 @@ ROBOT_IMAGE_NAME=${ROBOT_IMAGE_NAME:-"htw-berlin/devicer-robot"}
 WEB_IMAGE_NAME=${WEB_IMAGE_NAME:-"web"}
 WEB_IMAGE_PORT=${WEB_IMAGE_PORT:-"8080"}
 WEB_CONTAINER_NAME=${WEB_CONTAINER_NAME:-"devicereg_web_1"}
-ROBOT_IMAGE_NAME=${ROBOT_IMAGE_NAME:-"test"}
-ROBOT_CONTAINER_NAME=${ROBOT_CONTAINER_NAME:-"devicereg_test_1"}
 COMPOSE_NETWORK_NAME=${COMPOSE_NETWORK_NAME:-"devicereg_default"}
 
-# add --rm for local robot test execution
-docker run -e ROBOT_USER=${ROBOT_USER} -e WEB_IMAGE_NAME=${WEB_IMAGE_NAME} \
+docker run --rm -e ROBOT_USER=${ROBOT_USER} -e WEB_IMAGE_NAME=${WEB_IMAGE_NAME} \
     -e WEB_IMAGE_PORT=${WEB_IMAGE_PORT} -e ROBOT_BROWSER=${ROBOT_BROWSER} -e ROBOT_DISPLAY_RESOLUTION=${ROBOT_DISPLAY_RESOLUTION} \
     -v $(pwd)/${ROBOT_TESTS}:/robot-tests/devicer-testsuite/ -v $(pwd)/${ROBOT_OUTPUTDIR}:/robot-test-output \
-    --link ${WEB_CONTAINER_NAME}:${WEB_IMAGE_NAME} --link ${ROBOT_CONTAINER_NAME}:${ROBOT_IMAGE_NAME} -ti \
-    --net ${COMPOSE_NETWORK_NAME} ${ROBOT_IMAGE_NAME} run.sh
+    --link ${WEB_CONTAINER_NAME}:${WEB_IMAGE_NAME}  -ti --net ${COMPOSE_NETWORK_NAME} ${ROBOT_IMAGE_NAME} run.sh
 exit
