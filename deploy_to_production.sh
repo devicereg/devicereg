121f6f8d0ddd5c18bc18cdbd06395daa32654c8c
micken42 <michael_baumert@gmx.de>
2017-01-16 02:32:39 +0100

    Circleci test setup (#35)
    * Updated circle.yml
    
    * Updated circle.yml
    
    * Fixed test execution error on circleci machine
    
    * Updated circle.yml
    
    * Added a display resoulution environment variable for the robot tests
    
    * run_acceptance_tests.sh can now be used for local robot test execution
    
    * Execute test script instead of multiline command
    
    * Tested executing a bash script on circleci machine automatically
    
    * Some tests
    
    * Updated circle.yml
    
    * Updated circle.yml
    
    * Updated test execution bash script
    
    * Updated the test management

--- deploy_to_production.sh
+++ deploy_to_production.sh
@@ -5,7 +5,7 @@ git checkout -- .
 git pull
 echo "####### shut down the app and remove container #######"
 docker-compose down
-echo "####### remove the old docker images and container #######"
+echo "####### remove the old docker images and containers #######"
 docker rm $(docker ps -aq)
 docker rmi $(docker images -q -f dangling=true)
 docker rmi htw-berlin/devicer-robot htw-berlin/devicer-web
@@ -13,8 +13,4 @@ echo "####### building the new docker images #######"
 docker-compose build
 echo "####### starting the app #######"
 docker-compose up -d
-# Delete all stopped containers
-docker ps -q -f status=exited
-# Delete all dangling (unused) images
-
 exit
--- run_acceptance_tests.sh
+++ run_acceptance_tests.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
+ROBOT_DISPLAY_RESOLUTION=${ROBOT_DISPLAY_RESOLUTION:-"1280x1024x24"}
 ROBOT_USER=${ROBOT_USER:-"mustermax@mail.de"}
 ROBOT_BROWSER=${ROBOT_BROWSER:-"chrome"}
-ROBOT_OUTPUTDIR=${ROBOT_OUTPUTDIR:-"devicereg/app/static/test-output"}
-ROBOT_TESTS=${ROBOT_TESTS:-"devicereg/robot-tests/devicer-testsuite/"}
+ROBOT_OUTPUTDIR=${ROBOT_OUTPUTDIR:-"./app/static/test-output"}
+ROBOT_TESTS=${ROBOT_TESTS:-"./robot-tests/devicer-testsuite/"}
 ROBOT_IMAGE_NAME=${ROBOT_IMAGE_NAME:-"htw-berlin/devicer-robot"}
 
 WEB_IMAGE_NAME=${WEB_IMAGE_NAME:-"web"}
 WEB_IMAGE_PORT=${WEB_IMAGE_PORT:-"8080"}
 WEB_CONTAINER_NAME=${WEB_CONTAINER_NAME:-"devicereg_web_1"}
-ROBOT_IMAGE_NAME=${ROBOT_IMAGE_NAME:-"test"}
-ROBOT_CONTAINER_NAME=${ROBOT_CONTAINER_NAME:-"devicereg_test_1"}
 COMPOSE_NETWORK_NAME=${COMPOSE_NETWORK_NAME:-"devicereg_default"}
 
-docker run -e ROBOT_USER=${ROBOT_USER} -e WEB_IMAGE_NAME=${WEB_IMAGE_NAME} \
-    -e WEB_IMAGE_PORT=${WEB_IMAGE_PORT} -e ROBOT_BROWSER=${ROBOT_BROWSER} --rm \
+docker run --rm -e ROBOT_USER=${ROBOT_USER} -e WEB_IMAGE_NAME=${WEB_IMAGE_NAME} \
+    -e WEB_IMAGE_PORT=${WEB_IMAGE_PORT} -e ROBOT_BROWSER=${ROBOT_BROWSER} -e ROBOT_DISPLAY_RESOLUTION=${ROBOT_DISPLAY_RESOLUTION} \
     -v $(pwd)/${ROBOT_TESTS}:/robot-tests/devicer-testsuite/ -v $(pwd)/${ROBOT_OUTPUTDIR}:/robot-test-output \
-    --link ${WEB_CONTAINER_NAME}:${WEB_IMAGE_NAME} --link ${ROBOT_CONTAINER_NAME}:${ROBOT_IMAGE_NAME} -ti \
-    --net ${COMPOSE_NETWORK_NAME} ${ROBOT_IMAGE_NAME} run.sh
+    --link ${WEB_CONTAINER_NAME}:${WEB_IMAGE_NAME}  -ti --net ${COMPOSE_NETWORK_NAME} ${ROBOT_IMAGE_NAME} run.sh
 exit
